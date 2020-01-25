67396067ee7632739efcd532237fd4cb93f3fc65
Mightymaster99 <lessab99@gmail.com>
2020-01-24 23:19:13 -0600

    Main Menu, Level Stuff

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	// Start is called before the first frame update
+	void Start() {
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	public void GenerateLevel(int i) {
+		Debug.Log(i);
+		UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+	}
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class MainMenu : MonoBehaviour {
+
+	// The panel holding the level buttons
+	public GameObject levelPanel;
+
+	public GameObject buttonPrefab;
+
+	// Other buttons on the panel
+	public GameObject leftButton, rightButton;
+
+	// In charge of levels
+	public int currentPage = 0;
+	public int totalPages = 0;
+	public int lastUnlockedLevel = 0;
+
+	// Setting up initial state
+	public void Start() {
+		// Create the document in charge of this if it doesn't exist
+		if (!File.Exists(Path.Combine(Application.persistentDataPath, "player.dat"))) {
+			using (var writer = new StreamWriter(Path.Combine(Application.persistentDataPath, "player.dat"))) {
+				writer.WriteLine("0");
+			}
+		}
+
+		// Read the latest level
+		using (var reader = new StreamReader(Path.Combine(Application.persistentDataPath, "player.dat"))) {
+			lastUnlockedLevel = System.Convert.ToInt16(reader.ReadLine());
+		}
+
+		levelPanel.SetActive(false);
+		leftButton.GetComponent<Button>().interactable = false;
+		if (totalPages == 0) {
+			rightButton.GetComponent<Button>().interactable = false;
+		}
+
+		// Initializing the level buttons (15 per page)
+		// TO-DO: Manipulate this code to be fit for multiple pages
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 5; j++) {
+				GameObject newLevel = (GameObject)Instantiate(buttonPrefab, levelPanel.transform);
+				newLevel.transform.localPosition = new Vector2(-250 + 125 * j, 120 - 120 * i);
+				newLevel.transform.GetComponentInChildren<Text>().text = (i * 5 + j + 1).ToString();
+				newLevel.GetComponent<Button>().onClick.AddListener(() => LoadLevel(System.Convert.ToInt32(newLevel.transform.GetComponentInChildren<Text>().text)));
+
+				if (i * 5 + j + 1 > lastUnlockedLevel + 1) {
+					newLevel.GetComponent<Button>().interactable = false;
+				}
+			}
+		}
+	}
+
+	#region Back and Next Buttons
+	public void PreviousPage() {
+		currentPage--;
+		if (currentPage == 0) {
+			leftButton.GetComponent<Button>().interactable = false;
+		}
+
+		if (totalPages != 0) {
+			rightButton.GetComponent<Button>().interactable = true;
+		}
+	}
+
+	public void NextPage() {
+		currentPage++;
+
+		if (currentPage == totalPages) {
+			rightButton.GetComponent<Button>().interactable = false;
+		}
+
+		leftButton.GetComponent<Button>().interactable = true;
+	}
+	#endregion
+
+	public void ShowLevels() {
+		levelPanel.SetActive(true);
+	}
+
+	public void LoadLevel(int i) {
+		GameObject.Find("LevelManager").GetComponent<LevelManager>().GenerateLevel(i);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+
+}
