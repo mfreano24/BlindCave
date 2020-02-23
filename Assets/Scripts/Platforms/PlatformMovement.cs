df9f6a8915d8da738c081aec45c3bb0920972c13
Nicholas Vacek <nicholasvacek17@tamu.edu>
2020-02-23 13:22:57 -0600

    Have really basic platform movement

---
+++ Assets/Scripts/Platforms/PlatformMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlatformMovement : MonoBehaviour
+{
+    //private
+    private Vector3 startPosition;
+    
+    //public
+    public float amplitude = .5f;
+    public float frequency = 1f;
+
+    public enum movementType {
+        Horizontal, Vertical, Diagonal
+    };  
+    public movementType moveType; 
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (moveType)
+        {
+            case movementType.Horizontal:
+                Debug.Log("Horizontal");
+                startPosition = transform.position;
+                startPosition.x += Mathf.Sin(Time.time * frequency) * amplitude * Time.deltaTime;
+                transform.position = startPosition;
+                break;
+            case movementType.Vertical:
+                Debug.Log("Vertical");
+                startPosition = transform.position;
+                startPosition.y += Mathf.Sin(Time.time * frequency) * amplitude * Time.deltaTime;
+                transform.position = startPosition;
+                break;
+            case movementType.Diagonal:
+                Debug.Log("Diagonal");
+                break;
+        }
+
+    }
+}
