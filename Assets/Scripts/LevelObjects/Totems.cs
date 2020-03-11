10c35d05afa6efc7e088106d118ab14731dcfc1e
Nicholas Vacek <nicholasvacek17@tamu.edu>
2020-03-11 18:11:17 -0500

    Drop for totems working, removed extra comments

--- Assets/Scripts/LevelObjects/Totems.cs
+++ Assets/Scripts/LevelObjects/Totems.cs
@@ -60,41 +60,6 @@ public class Totems : MonoBehaviour
             {
                 Debug.Log("Parent component null");
             }
-
-            //if(other.CompareTag("Player"))
-            //{
-            //    Debug.Log("Other Tag is player");
-            //}
-            //else
-            //{
-            //    Debug.Log("Not the player");
-            //}
-            //if(player != null)
-            //{
-            //    Debug.Log("Player not null");
-            //    int directionFacing = player.GetComponent<DebugMovement>().directionFacing;
-            //    Debug.Log("directionFacing: " + directionFacing);
-            //    this.transform.position = new Vector3(other.gameObject.transform.position.x + directionFacing,
-            //        other.gameObject.transform.position.y,
-            //        other.gameObject.transform.position.z);
-            //}
-            //else
-            //{
-            //    Debug.Log("Player is null");
-            //}
-
-
-            //var PlayerObject = other.GetComponent<DebugMovement>();
-            //int directionFacing = PlayerObject.GetDirectionFacing();
-            //Debug.Log("Direction Facing: " + directionFacing);
-            //if(other.gameObject.GetComponent<DebugMovement>() != null)
-            //{
-            //    Debug.Log("Other Component is not null");
-            //}
-            //else
-            //{
-            //    Debug.Log("Other Component is null");
-            //}
             //Debug.Log("Other object direction facing: " + other.gameObject.GetComponent<DebugMovement>().directionFacing);
             //this.transform.position = new Vector3(other.gameObject.transform.position.x + other.gameObject.GetComponent<DebugMovement>().directionFacing, 
             //    other.gameObject.transform.position.y , 
