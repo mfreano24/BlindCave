df7e7dc57b686a083b3f4eb05e7cfb2694e600b9
Nicholas Vacek <nicholasvacek17@tamu.edu>
2020-03-22 01:34:24 -0500

    Started working on changing the color of objects

--- Assets/Scripts/LevelObjects/LevelButtons.cs
+++ Assets/Scripts/LevelObjects/LevelButtons.cs
@@ -7,7 +7,10 @@ public class LevelButtons : MonoBehaviour
 
 
     private bool platformRotating;
-    private bool buttonPressed;
+    private bool buttonPressed = false;
+    private bool triggeredEffect;
+
+    private Color oldColor;
 
 
     public enum buttonType
@@ -21,7 +24,7 @@ public class LevelButtons : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InvokeRepeating("changeColor", 1.0f, 2.0f);
     }
 
     // Update is called once per frame
@@ -31,4 +34,42 @@ public class LevelButtons : MonoBehaviour
      
         
     }
+
+    private void changeColor()
+    {
+        GameObject childObject = transform.GetChild(0).gameObject;
+        if(childObject == null)
+        {
+            Debug.Log("The child is NULL");
+        }
+        else
+        {
+            SpriteRenderer renderer = childObject.GetComponent<SpriteRenderer>();
+            if(renderer!= null)
+            {
+                triggeredEffect = false;
+                if ((buttonPressed == false) && (triggeredEffect == false)) {
+                    oldColor = renderer.color;
+
+                    renderer.color = Color.red;
+
+                    buttonPressed = true;
+                    triggeredEffect = true;
+
+                }
+                else if((buttonPressed == true) && (triggeredEffect == false))
+                {
+                    renderer.color = oldColor;
+
+                    buttonPressed = false;
+                    triggeredEffect = true;
+                }
+            }
+            else
+            {
+                Debug.Log("The sprite renderer of the child is null");
+            }
+        }
+    }
+
 }
